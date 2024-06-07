ce0fc712042ff4c0270177cc909183fd068b6818
SyahrialZky <syahrialrizky04@gmail.com>
2024-06-07 22:41:25 +0700

    init project

--- resources/views/absen-peserta/welcome.blade.php
+++ resources/views/absen-peserta/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Home')
 
 @section('content')
+
     <section class="mx-auto max-w-screen-xl">
         
         <div id="alert-4" class="mt-10 flex items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
